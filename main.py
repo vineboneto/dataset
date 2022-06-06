108695d2163842b040b98f595f6f516b07d7e279
vineboneto <vineboneto@gmail.com>
2022-06-06 16:10:16 -0300

    refactor: change to elif

--- main.py
+++ main.py
@@ -15,30 +15,30 @@ for idx in list(df.index):
     bmi = df.loc[idx, 'bmi']
     if gender == 'Male':
         df.loc[idx, 'gender'] = 0
-    if gender == 'Female':
+    elif gender == 'Female':
         df.loc[idx, 'gender'] = 1
-    if gender == 'Other':
+    else:
         df.loc[idx, 'gender'] = 2
 
     if ever_married == 'Yes':
         df.loc[idx, 'ever_married'] = 1
-    if ever_married == 'No':
+    else:
         df.loc[idx, 'ever_married'] = 0
 
     if work_type == 'children':
         df.loc[idx, 'work_type'] = 1
-    if work_type == 'Govt_job':
+    elif work_type == 'Govt_job':
         df.loc[idx, 'work_type'] = 2
-    if work_type == 'Never_worked':
+    elif work_type == 'Never_worked':
         df.loc[idx, 'work_type'] = 3
-    if work_type == 'Private':
+    elif work_type == 'Private':
         df.loc[idx, 'work_type'] = 4
-    if work_type == 'Self-employed':
+    else:
         df.loc[idx, 'work_type'] = 5
 
     if residence_type == 'Rural':
         df.loc[idx, 'Residence_type'] = 0
-    if residence_type == 'Urban':
+    else:
         df.loc[idx, 'Residence_type'] = 1
 
     if np.isnan(bmi):
@@ -46,11 +46,11 @@ for idx in list(df.index):
 
     if smoking_status == 'formerly smoked':
         df.loc[idx, 'smoking_status'] = 0
-    if smoking_status == 'never smoked':
+    elif smoking_status == 'never smoked':
         df.loc[idx, 'smoking_status'] = 1
-    if smoking_status == 'smokes':
+    elif smoking_status == 'smokes':
         df.loc[idx, 'smoking_status'] = 2
-    if smoking_status == 'Unknown':
+    else:
         df.loc[idx, 'smoking_status'] = 3
 
 df.to_csv(path_or_buf=r'./healthcare-dataset-stroke-data-adapter.csv', header=None)
